8e2a297b023c1e6ae6991183818465f4801aceb0
Francesco Favero <francesco.favero@cancer.org.uk>
2013-11-27 14:59:15 +0000

    next.R: Add seq2PyClone conversion function

--- R/next.R
+++ R/next.R
@@ -27,3 +27,18 @@ subclonal.matrix <- function(mut.tab, cellularity = seq(0.1, 1, 0.05), ploidy, a
   colnames(res) <- cellularity
   res
 }
+
+sequenza2PyClone <- function(mut.tab, seg.cn, sample.id, norm.cn = 2) { 
+  mut.tab <- cbind(mut.tab[,c('chromosome', 'n.base', 'good.s.reads','F', 'mutation')], CNt = NA, A = NA, B = NA)
+  for (i in 1:nrow(seg.cn)) {
+     pos.filt <- mut.tab$chromosome == seg.cn$chromosome[i] & mut.tab$n.base >= seg.cn$start.pos[i] & mut.tab$n.base <= seg.cn$end.pos[i]
+     mut.tab[pos.filt, c("CNt", "A", "B")] <- seg.cn[i, c("CNt", "A", "B")]
+  }
+  id          <- paste(sample.id, mut.tab$chromosome, mut.tab$n.base, sep = ':')
+  var.counts  <- round(mut.tab$good.s.reads * mut.tab$F, 0)
+  nor.counts  <- mut.tab$good.s.reads - var.counts
+  pyclone.tsv <- data.frame(mutation_id = id, ref_counts = nor.counts, var_counts = var.counts,
+                            normal_cn = norm.cn,	minor_cn = mut.tab$B, major_cn = mut.tab$A,
+                            variant_case = sample.id,	variant_freq = mut.tab$F,	genotype = mut.tab$mutation)
+  na.exclude(pyclone.tsv)
+}
