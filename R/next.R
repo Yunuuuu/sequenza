24d2a4d7ac6c1793934ee6247bfd39da04039bc4
Francesco Favero <francesco.favero@cancer.org.uk>
2014-01-29 19:16:45 +0000

    next.R: revive the subclonal.matrix function... it actually seems to work a bit.

--- R/next.R
+++ R/next.R
@@ -1,32 +1,32 @@
-# subclonal.matrix <- function(mut.tab, cellularity = seq(0.1, 1, 0.05), ploidy, avg.depth.ratio, mc.cores = 2){
-# 
-#   mut.types.list <- lapply(X = 1:nrow(mut.tab),
-#                            FUN = function(x) {
-#                              types.matrix(CNn = mut.tab[x, 'CNn'],
-#                                           CNt.min = mut.tab[x, 'CNt'],
-#                                           CNt.max = mut.tab[x, 'CNt'])})
-#   mut.cloanlity <- function(F, depth.t, types, cellularity, ploidy, avg.depth.ratio) {
-#     theorethic <- model.points(cellularity = cellularity,
-#                                ploidy = ploidy,
-#                                types = types,
-#                                avg.depth.ratio = avg.depth.ratio)
-#     max(mufreq.dpois(mufreq = F, mufreq.model = theorethic[, 1], depth.t = depth.t),na.rm = TRUE)
-#   }
-#   res <- mclapplyPb (mc.cores = mc.cores, X = 1:nrow(mut.tab),
-#                      FUN = function (i) {
-#                              sapply(X = cellularity, FUN = function(x) {
-#                                 mut.cloanlity(F = mut.tab$F[i],
-#                                            depth.t = mut.tab$good.s.reads[i],
-#                                            types = mut.types.list[[i]],
-#                                            cellularity = x, ploidy = ploidy,
-#                                            avg.depth.ratio = avg.depth.ratio)
-#                            })
-#                          })
-#   res <- do.call(rbind, res)
-#   res <- res/rowSums(res)
-#   colnames(res) <- cellularity
-#   res
-# }
+subclonal.matrix <- function(mut.tab, cellularity = seq(0.1, 1, 0.05), ploidy, avg.depth.ratio, mc.cores = 2){
+
+  mut.types.list <- lapply(X = 1:nrow(mut.tab),
+                           FUN = function(x) {
+                             types.matrix(CNn = mut.tab[x, 'CNn'],
+                                          CNt.min = mut.tab[x, 'CNt'],
+                                          CNt.max = mut.tab[x, 'CNt'])})
+  mut.cloanlity <- function(F, depth.t, types, cellularity, ploidy, avg.depth.ratio) {
+    theorethic <- model.points(cellularity = cellularity,
+                               ploidy = ploidy,
+                               types = types,
+                               avg.depth.ratio = avg.depth.ratio)
+    max(sequenza:::mufreq.dpois(mufreq = F, mufreq.model = theorethic[, 1], depth.t = depth.t),na.rm = TRUE)
+  }
+  res <- sequenza:::mclapplyPb (mc.cores = mc.cores, X = 1:nrow(mut.tab),
+                     FUN = function (i) {
+                             sapply(X = cellularity, FUN = function(x) {
+                                mut.cloanlity(F = mut.tab$F[i],
+                                           depth.t = mut.tab$good.s.reads[i],
+                                           types = mut.types.list[[i]],
+                                           cellularity = x, ploidy = ploidy,
+                                           avg.depth.ratio = avg.depth.ratio)
+                           })
+                         })
+  res <- do.call(rbind, res)
+  res <- res/rowSums(res)
+  colnames(res) <- cellularity
+  res
+}
 
 sequenza2PyClone <- function(mut.tab, seg.cn, sample.id, norm.cn = 2) {
   mut.tab <- cbind(mut.tab[,c('chromosome', 'n.base', 'good.s.reads','F', 'mutation')], CNt = NA, A = NA, B = NA)
