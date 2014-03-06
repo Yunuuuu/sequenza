5840f3caf955932246f3aec74df926fd71c7e832
Francesco Favero <francesco.favero@cancer.org.uk>
2014-03-06 11:26:14 +0000

    model.R: expected.baf: revers to previous functionality, considering the depth is OK but the actual approach can lead to wrong  alleles CN calls (overestimate LOH and so on)

--- R/model.R
+++ R/model.R
@@ -65,15 +65,15 @@ theoretical.baf <- function(CNn, CNt, cellularity) {
 }
 
 expected.baf <- function(depth, ...) {
-  baf      <- theoretical.baf(...)
-  baf.binom <- function(BAF, depth, by = 0.001){
-     bafs     <- seq(from = 0, to = 0.5, by = by)
-      curve    <- dbinom(round(depth*BAF,0), depth, bafs)
-      weighted.mean(bafs,curve)
-  }
-  baf$BAF <- mapply(FUN = baf.binom, baf$BAF,
-                                   MoreArgs = list(depth))
-  baf
+   baf      <- theoretical.baf(...)
+   #   baf.binom <- function(BAF, depth, by = 0.001){
+   #      bafs     <- seq(from = 0, to = 0.5, by = by)
+   #       curve    <- dbinom(round(depth*BAF,0), depth, bafs)
+   #       weighted.mean(bafs,curve)
+   #   }
+   #   baf$BAF <- mapply(FUN = baf.binom, baf$BAF,
+   #                                    MoreArgs = list(depth))
+   baf
 }
 
 baf.model.points <- function (cellularity, ploidy, avg.depth.ratio,
