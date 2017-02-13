479030f1a9f1008d874e511ce2a54a66ed6dfad2
Francesco Favero <francesco.favero@bric.ku.dk>
2017-02-13 15:50:40 +0100

    add more model tests

--- tests/testthat/test.model.R
+++ tests/testthat/test.model.R
@@ -27,3 +27,29 @@ test_that("Testing theoretical mufreq", {
     expect_equal(round(r2, 3), 0.286)
     expect_equal(r3, 0)
 })
+
+test_that("Testing baf types matrix", {
+    x <- 3
+    CNts <- do.call(c, sapply(1:x, FUN = function(x) rep(x, x)))
+    Bs <- do.call(c, sapply(1:x, FUN = function(x) {
+            y <- x:1
+            z <-  rep(x, x)
+            z - y
+        }
+    ))
+    mat_b <- cbind(CNn = rep(2, length(Bs) + 1),
+                   CNt = c(0, CNts), B = c(0, Bs))
+    mat_b <- mat_b[mat_b[, "B"] <= mat_b[, "CNt"] / 2, ]
+    r1 <- baf.types.matrix(CNt.min = 0, CNt.max = 3, CNn = 2)
+    expect_equal(r1, mat_b)
+})
+
+test_that("Testing mufreq types matrix", {
+    x <- 3
+    CNts <- do.call(c, sapply(1:x, FUN = function(x) rep(x, x + 1)))
+    Mts <- do.call(c, sapply(1:x, FUN = function(x) 0:x))
+    mat_m <- cbind(CNn = rep(2, length(Mts) + 1),
+                   CNt = c(0, CNts), Mt = c(0, Mts))
+    r1 <- mufreq.types.matrix(CNt.min = 0, CNt.max = 3, CNn = 2)
+    expect_equal(r1, mat_m)
+})
