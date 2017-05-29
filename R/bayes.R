340a65a8ac294f44c7543d4c114bdfe02be1b30e
Francesco Favero <francesco.favero@bric.ku.dk>
2017-05-29 23:24:53 +0200

    add mufreq fit frunctions

--- R/bayes.R
+++ R/bayes.R
@@ -122,3 +122,73 @@ baf.model.fit <- function(cellularity = seq(0.3, 1, by = 0.01),
     znorm <- exp(z - LogSumLik)
     list(ploidy = x, cellularity = y, lpp = znorm)
 }
+
+mufreq.bayes <- function(mufreq, depth.ratio, cellularity,
+    ploidy, avg.depth.ratio, weight.mufreq = 100, weight.ratio = 100,
+    CNt.min = 1, CNt.max = 7, CNn = 2,
+    priors.table = data.frame(CN = CNt.min:CNt.max, value = 1)) {
+    mufreq.tab <- data.frame(F = mufreq, ratio = depth.ratio,
+        weight.mufreq = weight.mufreq, weight.ratio = weight.ratio)
+    mufreq_types <- mufreq.types.matrix(CNt.min = CNt.min,
+        CNt.max = CNt.max, CNn = CNn)
+    model.pts <- mufreq.model.points(cellularity = cellularity,
+        ploidy = ploidy, mufreq_types = mufreq_types,
+        avg.depth.ratio = avg.depth.ratio)
+    model.pts <- cbind(mufreq_types, model.pts)
+    rows.x <- 1:nrow(mufreq.tab)
+    priors <- rep(1, nrow(model.pts))
+    for (i in 1:nrow(priors.table)) {
+        priors[model.pts$CNt ==
+            priors.table$CN[i]] <- priors.table$value[i]
+    }
+    priors <- priors / sum(priors)
+    bayes.fit <- function (x, mat, model.pts, priors) {
+        test.ratio <- model.pts$depth.ratio
+        test.mufrq <- model.pts$mufreqs
+        min.offset <- 1e-323
+        score.r <- depth.ratio.dbinom(size = mat[x, ]$weight.ratio,
+            depth.ratio = mat[x, ]$ratio, test.ratio)
+        score.m <- mufreq.dbinom(mufreq = mat[x, ]$F,
+            depth.t = mat[x, ]$weight.mufreq, test.mufrq)
+        score.r <- score.r * priors
+        score.m <- score.m
+        post.model <- score.r * score.m
+        post.model[post.model == 0] <- min.offset
+        res.cn <- model.pts$CNt[which.max(score.r)]
+        idx.pts <- model.pts$CNt == res.cn
+        model.lik <- cbind(model.pts[idx.pts, 1:3], log(post.model[idx.pts]))
+        if (is.null(dim(model.lik))) {
+            max.post <- model.lik
+        } else {
+            max.post <- model.lik[which.max(model.lik[,4]),]
+        }
+        max.post
+    }
+    types.L <- mapply(FUN = bayes.fit, rows.x, MoreArgs = list(
+        mat = mufreq.tab, model.pts = model.pts,
+        priors = priors), SIMPLIFY = FALSE)
+   types.L <- do.call(rbind, types.L)
+   colnames(types.L) <- c("CNn", "CNt", "Mt", "LPP")
+   types.L
+}
+
+mufreq.model.fit <- function(cellularity = seq(0.3, 1, by = 0.01),
+    ploidy = seq(1, 7, by = 0.1),  mc.cores = getOption("mc.cores", 2L), ...) {
+   result <- expand.grid(ploidy = ploidy, cellularity = cellularity,
+        KEEP.OUT.ATTRS = FALSE)
+    fit.cp <- function(ii) {
+        L.model <- mufreq.bayes(cellularity = result$cellularity[ii],
+            ploidy = result$ploidy[ii], ...)
+        sum(L.model[, 4])
+    }
+    bayes.res <- mclapplyPb(X = 1:nrow(result), FUN = fit.cp,
+        mc.cores = mc.cores)
+    result$LPP <- unlist(bayes.res)
+    z <- tapply(result$LPP, list(result$ploidy, result$cellularity), mean)
+    x <- as.numeric(rownames(z))
+    y <- as.numeric(colnames(z))
+    max.lik <- max(result$LPP, na.rm = TRUE)
+    LogSumLik <- log(sum(exp(result$LPP - max.lik))) + max.lik
+    znorm <- exp(z - LogSumLik)
+    list(ploidy = x, cellularity = y, lpp = znorm)
+}
