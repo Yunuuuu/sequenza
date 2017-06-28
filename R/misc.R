65d655edf53d071623c5686ffefa1f11c18bee94
Francesco Favero <francesco.favero@bric.ku.dk>
2017-06-28 11:13:13 +0200

    Remove unused functions

--- R/misc.R
+++ R/misc.R
@@ -3,24 +3,8 @@ dt2 <- function(x, df, ncp, log = FALSE, mean, sd) {
     dt(x2, df = df, ncp = ncp, log = log)
 }
 
-mclapplyPb <- function(X, FUN, mc.cores = getOption("mc.cores", 2L), ...) {
-    env <- environment()
-    pb_Total <- length(X) / mc.cores
-    counter <- 0
-    pb <- txtProgressBar(min = 0, max = pb_Total, style = 3)
-    wrapper <- function(...){
-        curVal <- get("counter", envir = env)
-        assign("counter", curVal + 1 ,envir = env)
-        setTxtProgressBar(get("pb", envir = env), curVal + 1)
-        FUN(...)
-    }
-    res <- mclapply(X, wrapper, mc.cores = mc.cores, ...)
-    close(pb)
-    res
-}
-
 split_chr_coord <- function (x) {
-    # Ensure that there is a start end coordinate
+    # Ensure that there is a start and a end coordinate
     split_chr <- strsplit(x, split = ":")[[1]]
     chromosome <- split_chr[1]
     start_end <- split_chr[2]
